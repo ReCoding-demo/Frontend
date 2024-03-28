4c818bc9e8663d38173b459a313bcd9f6cf53ffe
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-03-28 11:03:01 +0900

    [Design] 로그인 페이지 제작

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,22 @@
 const LoginPage = () => {
-  return <div>LoginPage입니다</div>;
+  const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  return (
+    <div className=" pt-32 flex flex-col gap-10 items-center m-auto">
+      <img src="/icons/logo-typo.svg" className="w-[135px]" />
+      <p className=" text-center text-gray100 headline-md">
+        네카라쿠배 개발자에게
+        <br />
+        지금 바로 코드리뷰를 받아보세요
+      </p>
+      <button
+        className="max-w-[360px] w-[95%] py-4 flex justify-center items-center gap-2 bg-[#FDDC3F] rounded-lg"
+        onClick={onKaKaoHandler}
+      >
+        <img src="/icons/kakao.svg" />
+        <p className="text-gray100 label-lg-pro">카카오로 3초만에 시작하기</p>
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
