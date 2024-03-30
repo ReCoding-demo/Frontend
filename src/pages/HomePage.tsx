1f8def593e71971f0dcde66e3a8e68047f72a2ec
minseo-hong <mshong1007@gmail.com>
2024-03-30 13:12:57 +0900

    [Refactor] 개발자 수 페이지에 반영

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <div className="px-5">
       <main className="m-auto max-w-screen-lg">
         <p className="my-6 font-medium text-gray80">
-          <b className="text-green70 font-semibold">10</b>명의 현직 개발자가
-          기다리고 있어요.
+          <b className="text-green70 font-semibold">{developerList.length}</b>
+          명의 현직 개발자가 기다리고 있어요.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-9">
           {developerList.map((developer) => (
