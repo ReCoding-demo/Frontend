e81a642477ea00b952d01c20b90b8f3aca728422
minseo-hong <77859988+minseo-hong@users.noreply.github.com>
2024-04-04 14:20:30 +0900

    [#11] 개발자 조회 페이지 카드 그리드 리스트 만들기 (#12)
    * [Design] 개발자 조회 페이지 카드 그리드 리스트 제작
    
    * [Refactor] 개발자 데이터 파일로 분리
    
    * [Refactor] 개발자 수 페이지에 반영
    
    * [Fix] 코드 리뷰 수정사항 반영
    
    * [Design] 워딩 및 tailwind 색 클래스 변경 반영
    
    * [Refactor] Tag 컴포넌트 태그 이름 props 리팩토링

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,23 @@
+import DeveloperCard from '../components/home/DeveloperCard';
+import { developerList } from '../data/developer';
+
 const HomePage = () => {
-  return <div className="text-3xl font-bold">HomePage입니다</div>;
+  return (
+    <div className="px-5">
+      <main className="m-auto max-w-screen-lg">
+        <p className="my-6 label-lg text-gray80">
+          조건에 맞는{' '}
+          <b className="text-green-70 label-lg-pro">{developerList.length}</b>
+          명의 현직 개발자가 기다리고 있어요.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-9">
+          {developerList.map((developer) => (
+            <DeveloperCard key={developer.id} developer={developer} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
